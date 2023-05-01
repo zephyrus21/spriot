dd1f533197bc4814190ac15bb54be5a33405f215
Piyush Pandey <pandey.piyus@gmail.com>
2023-05-01 10:59:12 +0530

    gg

--- src/main/java/com/zephyrus/spriot/entities/Department.java
+++ src/main/java/com/zephyrus/spriot/entities/Department.java
@@ -5,12 +5,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
--- src/test/java/com/zephyrus/spriot/SpriotApplicationTests.java
+++ src/test/java/com/zephyrus/spriot/SpriotApplicationTests.java
@@ -1,13 +1,8 @@
 package com.zephyrus.spriot;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpriotApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
